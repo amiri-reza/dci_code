101341f0db1eb4691036a94be572ba93af9364af
amiri-reza <amiri.reza68@yahoo.com>
2022-07-22 14:14:43 +0200

    Excercises1

--- Simple Print Task.py
+++ Simple Print Task.py
@@ -1,10 +1,12 @@
 s_name = input("Please write your name: ")
 s_lastname = input("Please write your last name: ")
+s_email = input("Please enter your email address: ")
 s_number = int(input("Please write your student number: "))
 s_class = input("Please write your class code: ")
 
 print("Student Records")
 print(s_name + " " + s_lastname)
+print(s_email)
 print("Student Number: ", s_number)
 print("Class: ", s_class)
 
