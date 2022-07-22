0935c93d87183c1e872b0496aebb343b678f20c7
amiri-reza <amiri.reza68@yahoo.com>
2022-07-22 09:01:23 +0200

    Simple Print Task

---
+++ stringtest.py
@@ -0,0 +1,13 @@
+s_name = input("Please write your name: ")
+s_lastname = input("Please write your last name: ")
+s_number = int(input("Please write your student number: "))
+s_class = input("Please write your class code: ")
+
+print("Student Records")
+print(s_name + " " + s_lastname)
+print("Student Number: ", s_number)
+print("Class: ", s_class)
+
+
+
+
