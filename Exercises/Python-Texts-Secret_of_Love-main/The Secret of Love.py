db3fdc3825af7544ed92586ac3e118e1a4c81e3f
amiri-reza <amiri.reza68@yahoo.com>
2022-07-24 21:47:09 +0200

    excercises-in-holiday

---
+++ Exercises/Python-Texts-Secret_of_Love-main/The Secret of Love.py
@@ -0,0 +1,30 @@
+secret = input("Please tell me the secret: ")
+
+def function(secret):
+  return secret[::-1]
+
+
+
+print(secret)
+
+
+
+
+
+
+
+
+#mytxt = my_function("I wonder how this text looks like backwards")
+
+#print(mytxt)
+
+
+
+
+
+
+
+#secret = input("Type your secret: ")
+
+
+#print(secret)
--- Exercises/firstex.py
+++ Exercises/firstex.py
@@ -1,6 +1,8 @@
 emails = ["123whatever@gmail.com","456idontcare@gmail.com","789istilldontmind@hotmail.de","forrealbruvwhatever@gmx.de"]
 
-usernames = []
+
+usernames = emails[0][:11], emails[1][:12], emails[2][:17], emails[3][:19]
+
 email_provider = []
 
 # please fill the two lists with the data from the "emails" list. Use string slicing and methods for extracting what you need
@@ -12,4 +14,4 @@ email_provider = []
 
 
 
-print()
+print(usernames)
