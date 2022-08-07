353b32a3ca37571c1fee4f4b8b5e3dd1fbd75d9a
amiri-reza <amiri.reza68@yahoo.com>
2022-08-07 19:55:02 +0200

    firstex

--- Exercises/firstex.py
+++ Exercises/firstex.py
@@ -1,17 +1,32 @@
 emails = ["123whatever@gmail.com","456idontcare@gmail.com","789istilldontmind@hotmail.de","forrealbruvwhatever@gmx.de"]
 
+username1 = emails[0]
+username2 = emails[1]
+username3 = emails[2]
+username4 = emails[3]
 
-usernames = emails[0][:11], emails[1][:12], emails[2][:17], emails[3][:19]
+username1_split = username1.split("@")[0]
+username2_split = username2.split("@")[0]
+username3_split = username3.split("@")[0]
+username4_split = username4.split("@")[0]
 
-email_provider = []
+
+domain1_split = username1.split("@")[1]
+domain2_split = username2.split("@")[1]
+domain3_split = username3.split("@")[1]
+domain4_split = username4.split("@")[1]
+
+print("Usernames: \n" + username1_split, username2_split, username3_split, username4_split)
+print("domain names: \n" + domain1_split, domain2_split, domain3_split, domain4_split,)
+
+#usernames = emails[0:4]
+#print(usernames)
+
+
+#email_provider = []
 
 # please fill the two lists with the data from the "emails" list. Use string slicing and methods for extracting what you need
 #example:
 #  test@mail.com
 # usernames = ["test"]
 # email_provider = ["mail.com"]
-
-
-
-
-print(usernames)
