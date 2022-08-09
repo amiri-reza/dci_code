ca0c71a6ce7171b8c7ff9ebb85d626f7a6dee808
amiri-reza <amiri.reza68@yahoo.com>
2022-08-09 15:33:53 +0200

    Exercise1 changes from the first day after holiday

---
+++ Exercises/Ex1/exercise.py
@@ -0,0 +1,76 @@
+# do exercise here
+# upload to github for portfolio
+# thank me later when your git graph is as green as the python money u gonna earn
+
+
+print("""
+--------------------------------------------------------------------
+Task 1
+--------------------------------------------------------------------""")
+
+Int = int(234)
+Float = float(43.12)
+Complex = complex(3,2)
+String = str("Hello")
+Boolean = bool(3)
+
+print(Int, Float, Complex, String, Boolean, sep="\n")
+
+
+print("""
+--------------------------------------------------------------------
+Task 2
+--------------------------------------------------------------------""")
+
+Num =123
+Float2 = 43.23
+Complex2 = complex(4,-1)
+String2 = "How are you"
+Boolean2 = bool(True)
+
+print(Num,type(Num), sep=(" is a type of "))
+print(Float2, type(Float2), sep=(" is a type of "))
+print(Complex2, type(Complex2), sep=(" is a type of "))
+print(String2, type(String2), sep=(" is a type of "))
+print(Boolean2, type(Boolean2), sep=(" is a type of "))
+#---------------------------------------------------------------------------------------------------------------------------------
+
+print("""
+--------------------------------------------------------------------
+Task 3
+--------------------------------------------------------------------""")
+#Task3
+print(isinstance(Num,int))
+print(isinstance(Float2,int))
+print(isinstance(Complex2,complex))
+print(isinstance(String2,str))
+print(isinstance(Boolean2,float))
+
+print("""
+--------------------------------------------------------------------
+Task 4
+--------------------------------------------------------------------""")
+
+print("is ", Num, "an instance of int?: ", isinstance(Num,int))
+print("is ", Float2, "an instance of bool?: ", isinstance(Float2,bool))
+print("is ", Complex2, "an instance of int?: ", isinstance(Complex2,complex))
+print("is ", "'"+String2+"'", "an instance of int?: ", isinstance(String,bool))
+print("is ", Boolean2, "an instance of int?: ", isinstance(Boolean2,float))
+
+print("""
+--------------------------------------------------------------------
+Task 5
+--------------------------------------------------------------------""")
+# This is an example of Block Comment.
+# Block comments are one type of comment.
+
+print("is ", Num, "an instance of int?: ", isinstance(Num,int))  # this is a inline comment.
+print("is ", Float2, "an instance of bool?: ", isinstance(Float2,bool))
+print("is ", Complex2, "an instance of int?: ", isinstance(Complex2,complex))
+print("is ", "'"+String2+"'", "an instance of int?: ", isinstance(String,bool))
+print("is ", Boolean2, "an instance of int?: ", isinstance(Boolean2,float))
+
+"""this is also a comment for documenting tasks.
+it can be multiline or in one line.
+"""
+
