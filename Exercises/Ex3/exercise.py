8e84717164b2230330bdd68dad8ba10907ae50f7
amiri-reza <amiri.reza68@yahoo.com>
2022-08-11 11:59:22 +0200

    3rd day with ex3 finished

--- Exercises/Ex3/exercise.py
+++ Exercises/Ex3/exercise.py
@@ -1,8 +1,8 @@
 # do exercise here
 # upload to github for portfolio
 # thank me later when your git graph is as green as the python money u gonna earn
-'''
 
+'''
 print("""
 --------------------------------------------------------------------
 Task 1
@@ -13,60 +13,105 @@ for input1 in range(0,3):
         print("Even Number")
     else:
         print("Odd Number")
+'''
 
 
 '''
-
 print("""
 --------------------------------------------------------------------
 Task 2
 --------------------------------------------------------------------""")
 
-input2 = input("Please enter a number between 1 to 3 for arguments in range() function: ")
-i = int(input2)
-
-if i == 1:
-    input3 = input("Please enter the range number: ")
-    j = int(input3)
-    for x in range(j):
-        print(x)
-elif i == 2:
-    input4 = input("Please enter starting number: ")
-    k = int(input4)
-    input5 = input("Please enter stopping number: ")
-    l = int(input5)
-    for y in range(k, l):
-        print(y)
-elif i == 3:
-    input6 = input("Please enter starting number: ")
-    m = int(input6)
-    input7 = input("Please enter stopping number: ")
-    n = int(input7)
-    input8 = input("Please enter step: ")
-    o = int(input8)
-    for y in range(m, n, o):
-        print(y)
 
 
 
+while True:
+
+    i = int(input("Please enter a number between 1 to 3 or '0' to end program: "))
+    if i == 0:
+        print("Bye bye!")
+        break
+    elif i == 1:
+        j = int(input("Please enter the range number: "))
+        for x in range(j+1):
+            print(x)
+    elif i == 2:
+        k = int(input("Please enter starting number: "))
+        l = int(input("Please enter stopping number: "))
+        for y in range(k, l+1):
+            print(y)
+    elif i == 3:
+        m = int(input("Please enter starting number: "))
+        n = int(input("Please enter stopping number: "))
+        o = int(input("Please enter step: "))
+        for z in range(m, n, o+1):
+            print(z)
+    else:
+        print("sorry, but you have to choose numbers between 1, 2 or 3! or '0' to end this.")
+'''
+
+'''
+print("""
+--------------------------------------------------------------------
+Task 3
+--------------------------------------------------------------------""")
 
 
-
+number = int(input("enter your number: "))
+for i in range(1, number+1):
     
-"""
-input3 = input("enter starting number: ")
-y = int(input3)
-
-input4 = input("enter stopping number: ")
-z = int(input4)
-
-input5 = input("enter steps: ")
-k = int(input5)
-
-
-for x in range(3, 20, 8):
-    print(x)
+    if number % i == 0:
+        print(i)
+'''
 
 
 
-    """
+
+'''
+print("""
+--------------------------------------------------------------------
+Task 4
+--------------------------------------------------------------------""")
+
+u = int(input("enter the number: "))
+
+if u > 1:
+    for t in range(2, u):
+        if u % t == 0:
+            print(u, "is not a prime number.")
+            break
+    else:
+            print(u, "is a prime number.")
+            
+else:
+    print("That is not a prime number!")
+'''
+
+
+'''
+print("""
+--------------------------------------------------------------------
+Task 5
+--------------------------------------------------------------------""")
+
+
+for x in range(1, 101):
+    if x % 15 == 0:
+        print("BuzzFizz")
+    elif x % 3 == 0:
+        print("Buzz")
+    elif x % 5 == 0:
+        print("Fizz")
+    else:
+        print(x)
+'''
+
+print("""
+--------------------------------------------------------------------
+Task 6
+--------------------------------------------------------------------""")
+
+for p in range(999, 2001):
+    if p % 7 == 0 and p % 5 != 0:
+        print(p)
+
---
+++ Exercises/testfile.py
@@ -0,0 +1,34 @@
+'''convert=float(25.4)
+while True:
+    print("*******MM*******")
+    user_input = input()
+    if user_input.lower() == "end":
+        break
+    else:
+        MM = float(user_input)
+        Results=float(MM/convert)
+        print("*****Inches*****")
+        print("%.3f" % Results)
+        print("%.4f" % Results)
+        print("%.5f" % Results)'''
+
+num = int(input("Enter a number: "))
+flag = False
+
+# prime numbers are greater than 1
+if num > 1:
+    # check for factors
+    for i in range(2, num):
+        if (num % i) == 0:
+            # if factor is found, set flag to True
+            flag = True
+            # break out of loop
+            break
+
+# check if flag is True
+if flag:
+    print(num, "is not a prime number")
+else:
+    print(num, "is a prime number")
+
+
