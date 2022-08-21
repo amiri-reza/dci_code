7e786e6a1f3cfade2d07c7fd33d986a7d407511b
amiri-reza <amiri.reza68@yahoo.com>
2022-08-21 22:31:53 +0200

    multiple choice story is finished.

--- Exercises/testfile.py
+++ Exercises/testfile.py
@@ -32,7 +32,46 @@ else:
     print(num, "is a prime number")
 
 '''
-g1 = "What is the biggest continent in the world?"
-g1o = "A: Africa  B: Europe  C: Asia   D: North America to quit, press Q"
+from textwrap import dedent
+import textwrap
 
-print(g1.center(100), g1o.center(100), sep="\n")
+x = "this is a sentence. i am very happy. my name is Reza"
+print(f"hello{'. '.join(map(lambda s: s.strip().capitalize(), x.split('.')))}")                           
+#x = input(dedent(y)).lower()
+
+#print(x)
+
+# questions = ["What is 1 + 1",
+#              "Who is the 45th president of the United States?",
+#              "True or False... The Toronto Maple Leafs have won 13 Stanley   Cups?",
+#              "What was the last year the Toronto Maple Leafs won the Stanley   Cup?",
+#              "True or False... The current Prime Minister of Canada is Pierre Elliot Tredeau?"]
+# answer_choices = ["a)1\nb)2\nc)3\nd)4\n:",
+#                   "a)Barack Obama\nb)Hillary Clinton\nc)Donald Trump\nd)Tom Brady\n:",
+#                   ":",
+#                   "a)1967\nb)1955\nc)1987\nd)1994\n:",
+#                   ":"]
+# correct_choices = ["b",
+#                    "c",
+#                    "t",
+#                    "a",
+#                    "f"]
+# answers = ["1 + 1 is 2",
+#            "The 45th president is Donald Trump",
+#            "",
+#            "The last time the Toronto Maple Leafs won the Stanley Cup was 1967",
+#            "The current Prime Minster of Canada is Justin Tredeau"]
+
+
+# def quiz():
+#     score = 0
+#     for question, choices, correct_choice, answer in zip(questions, answer_choices, correct_choices, answers):
+#         print(question)
+#         user_answer = input(choices).lower()
+#         if user_answer in correct_choice:
+#             print("Correct")
+#             score += 1
+#         else:
+#             print("Incorrect", answer)
+#     print(score, "out of", len(questions), "that is", float(score / len(questions)) * 100, "%")
+# quiz()
