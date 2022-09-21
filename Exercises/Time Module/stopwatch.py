f406b282e734c820ada1f5a232f42413489a8a5d
amiri-reza <amiri.reza68@yahoo.com>
2022-09-21 10:52:52 +0200

    small updates

--- Exercises/Time Module/stopwatch.py
+++ Exercises/Time Module/stopwatch.py
@@ -1,9 +1,33 @@
 import time
+import os
 
 
 starting_point = input("press Enter to start")
 start = time.time()
 print(time.ctime())
+
+
+                                            # making python wait for the user input (not working yet)
+# Flag = True
+# while Flag:
+#     try:
+#         print(time.ctime())
+#         print("Press Enter to end")
+#         User_inp = input()
+        
+#         if User_inp == "":
+#             pass
+#         else:
+#             end = time.time()
+#             Flag = False
+            
+#     except:
+#         print('except part') 
+
+
+
+
+
 
 
 ending_time = input("press Enter to end")
