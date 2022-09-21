7124047e5396076f6ad5a96afb8aa76c20de4ab6
amiri-reza <amiri.reza68@yahoo.com>
2022-09-21 10:52:45 +0200

    small updates

--- Exercises/Time Module/ProgressBar.py
+++ Exercises/Time Module/ProgressBar.py
@@ -5,22 +5,22 @@ import os
 starting_time = time.time()
 filled_sign = " "
 
-progress_bar = "--------------------------------------------------"
+progress_bar = 100*"-"
 
 
 n = 0
 while n < 101:
     os.system("clear")
     
-    decreasing_bar = progress_bar[0:-int(n/2)]
+    decreasing_bar = progress_bar[0:-int(n)]
     filled_sign += "#"
     if n == 0:
         print(f"#{progress_bar}- progress is {n} %.")
     else:
         print(f"{filled_sign}{decreasing_bar} progress is {n} %.")
-    time.sleep(0.1)
-    n += 2
+    time.sleep(0.05)
+    n += 1
 
 print("Download is finished!")
 finishing_time = time.time()
-print("Download Duration:", finishing_time-starting_time, "Seconds")
+print("Download Duration:", round(finishing_time-starting_time, 2), "Seconds")
